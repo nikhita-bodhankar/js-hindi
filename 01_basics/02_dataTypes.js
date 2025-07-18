7949fe0f449868916db7360edcd6032215a6c778
nikhita-bodhankar <158724568+nikhita-bodhankar@users.noreply.github.com>
2025-07-18 11:32:36 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all js code as a newer version
+
+// alert(3 + 3) we are using node.js not browser
+
+console.log(3 + 3)
+console.log("Nikhita")
+
+let name = "rani";
+let age = 24;
+let isLoggedIn = false;
+
+//number => 2 to power 53
+//bigint
+//string =""
+//boolean =>true/false
+//null => standalone value
+//undefined
+//symbol => unique
+
+//object
+
+console.log(typeof "nikki");
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
+
+
+
