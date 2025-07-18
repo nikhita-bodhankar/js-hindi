feabdd238119cad556355c8da7b6bbd519d38bb0
nikhita-bodhankar <158724568+nikhita-bodhankar@users.noreply.github.com>
2025-07-18 12:07:22 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+// let score = "33abc"
+// let score = null
+//  let score = true
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueNumber = Number(score)
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+
+//33 = number
+//33abc = NaN
+//true =1 , false =0
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "nikki"
+
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 =>true ; 0 => false
+//"" => false
+//"nikki" => true;
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
