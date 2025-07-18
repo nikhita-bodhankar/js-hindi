d59100f9e9fe5d4528b0510eb7b3850af282cbde
nikhita-bodhankar <158724568+nikhita-bodhankar@users.noreply.github.com>
2025-07-18 17:53:59 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,19 +17,69 @@
 
 // let isLoggedIn = 1
 // let isLoggedIn = ""
-let isLoggedIn = "nikki"
+// let isLoggedIn = "nikki"
 
-let booleanIsLoggedIn = Boolean (isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean (isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 //1 =>true ; 0 => false
 //"" => false
 //"nikki" => true;
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//*****************************operatios *****************************/
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%2);
+// console.log(2/2);
+
+let str1 = "Rani"
+let str2 = " nikki"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+ 2);
+// console.log(1 + "2" );
+// console.log("1"+ "2");
+// console.log("1"+ 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+//link to study
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
