823832aa1ec1d76a7f42d71361218eefbae220d6
nikhita-bodhankar <158724568+nikhita-bodhankar@users.noreply.github.com>
2025-07-20 16:56:07 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,46 @@
+// const name ="nikki "
+// const repoCount = 50
+
+// console.log(name + repoCount + " value");
+
+// console.log(`Hello my name is ${name} and repocount is ${repoCount}`);
+
+const gameName = new String('Rani-Nikki')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(1));
+console.log(gameName.indexOf('n'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+const newStringOne = "    nikki   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https/nikhitabodhankar%20gmail.com"
+console.log(url.replace("%","-"));
+console.log(url.includes("Rani"));
+console.log(url.includes("nikhita"));
+console.log(gameName.split("-"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
