115b7b560fea620f424dcb6cf2ae255021ed69c9
nikhita-bodhankar <158724568+nikhita-bodhankar@users.noreply.github.com>
2025-07-20 19:48:02 +0000

    arrays in js

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,45 @@
+//Array
+
+const myArray = [0,1,2,3,4,5]
+const myHeros = ["Shaktiman" , "Naagraj"]
+const myArray2 = new Array(1, 2, 3, 4)
+
+// console.log(myArray[1]);
+// console.log(myHeros[0]);
+
+//Array Methods
+
+// myArray.push(6)
+// myArray.push(7)
+// myArray.pop()
+// myArray.unshift(3)
+// myArray.shift()
+// console.log(myArray);
+// console.log(myArray.includes(3));
+// console.log(myArray.indexOf(9));
+// console.log(myArray.indexOf(3));
+
+// const newArray = myArray.join()
+// console.log(myArray);
+// console.log(newArray);
+
+//slice and spice
+
+console.log("A " ,myArray);
+
+console.log("B " ,myArray);
+let myn1 = myArray.slice(1,3)
+console.log(myn1);
+
+console.log("C " ,myArray);
+let myn2 = myArray.splice(1,3)
+console.log(myn1);
+
+console.log(myArray);
+
+
+
+
+
+
+
