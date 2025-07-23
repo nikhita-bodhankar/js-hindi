6206ecc3ec6ec4cafdfe6c25ddf585159772ce7e
nikhita-bodhankar <158724568+nikhita-bodhankar@users.noreply.github.com>
2025-07-23 04:04:30 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,19 @@
+var c = 300
+let a = 400
+
+if(true) {
+ let a = 10;
+ console.log("Inner:",a);
+ 
+//  const b = 20;
+ var c = 30;
+}
+
+console.log(a);
+// console.log(b);
+console.log(c);
+
+
+
+
+
