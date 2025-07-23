41af5572b9da963a00b7f1269946be41d7d8d865
nikhita-bodhankar <158724568+nikhita-bodhankar@users.noreply.github.com>
2025-07-23 04:57:16 +0000

    scopes2

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,15 +3,57 @@ let a = 400
 
 if(true) {
  let a = 10;
- console.log("Inner:",a);
+//  console.log("Inner:",a);
  
 //  const b = 20;
  var c = 30;
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+
+
+// function one(){
+//     const username = "nikki"
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);    
+//     }
+//     // console.log(website);
+//     two()
+// }
+// one()
+
+if(true){
+    const username = "rani"
+    if(username === "rani"){
+        const website = " youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+  }
+// console.log(username);
+
+//+++++++++++++++++++++++++++++++++++++++++++ interesting +++++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(addOne(5)); // can access
+
+function addOne(num){
+    return num + 1
+}
+// console.log(addOne(5));
+
+
+
+
+console.log(addTwo(5)); // can not access 
+const addTwo = function(num){
+    return num + 2
+}
+// console.log(addTwo(5));
+
 
 
 
