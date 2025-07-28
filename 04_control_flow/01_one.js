cd72621a7a690927284c9167a8bdbcdb233e447b
nikhita-bodhankar <158724568+nikhita-bodhankar@users.noreply.github.com>
2025-07-28 08:52:03 +0000

    loops

---
+++ 04_control_flow/01_one.js
@@ -0,0 +1,73 @@
+//if
+
+const isUserLoggedIn = true
+
+if(isUserLoggedIn){
+
+}
+
+if(2==="2"){
+    console.log("executed");
+    
+}
+// console.log("run");
+
+if(2 != 3){
+    // console.log("executed true");
+}
+
+// const temperature = 51
+// if(temperature < 50){
+//     console.log("Temperature is less than 50");
+    
+// }
+// console.log("Temperature is greater than 50");
+
+// const temperature = 42
+// if(temperature < 50){
+//     console.log("Temperature is less than 50");
+    
+// }
+// else{
+//     console.log("Temperature is greater than 50");
+
+// }
+
+// const score = 200
+// if (score > 100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+//     console.log(`User power: ${power}`);
+
+
+const balance = 1000
+// if(balance > 500) console.log("test");
+
+// if(balance < 500){
+//     console.log("balance is less than 500");
+    
+// }else if(balance < 720){
+//     console.log("balance is less than 720");
+    
+// }else if(balance < 900){
+//     console.log("balance is less than 900");
+// }
+// else{
+//     console.log("balance is less than 1200");
+
+// }
+
+const userLoggedIn = true;
+const userDebitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+// if(userLoggedIn && userDebitCard)
+// {
+//     console.log("Allow to buy course");
+    
+// }
+
+if (loggedInFromGoogle || loggedInFromEmail){
+    console.log("user logged in");
+}
