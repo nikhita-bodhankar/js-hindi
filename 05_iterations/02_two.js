625a4cd0d3737811ec310cd1d5cf7b7b4db0d0e9
nikhita-bodhankar <158724568+nikhita-bodhankar@users.noreply.github.com>
2025-07-31 03:51:35 +0000

    while nd do while

---
+++ 05_iterations/02_two.js
@@ -0,0 +1,25 @@
+//while loop
+
+let index = 0
+while (index <=10){
+    console.log(`the value of index is ${index}`);
+    index = index + 2
+    
+}
+
+let myArray = ["nikki" , "RR" , "NN"]
+let arr =0
+while (arr < myArray.length) {
+    console.log(`Array is ${myArray[arr]}`);
+    arr = arr+1
+    
+    
+}
+
+let score = 0;
+do {
+    console.log(`score is ${score}`);
+    score++
+    
+    
+} while (score <=10);
