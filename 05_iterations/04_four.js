e9c82c18bac262f16662b9bbf0440ad455482270
nikhita-bodhankar <158724568+nikhita-bodhankar@users.noreply.github.com>
2025-07-31 08:25:34 +0000

    high order iterations

---
+++ 05_iterations/04_four.js
@@ -0,0 +1,17 @@
+// const myObject = {
+//     js: 'Javascript',
+//     rb: 'ruby',
+//     cpp: 'c++'
+// }
+
+// for (const key in myObject) {
+    
+//       console.log(`${key}  longform is ${myObject[key]}`);
+        
+    
+// }
+const programming = ["js","php","c#"]
+for (const key in programming) {
+   console.log(programming[key]);
+   
+}
